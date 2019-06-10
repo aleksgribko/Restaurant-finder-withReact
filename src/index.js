822a10e742ff640749470f715c7d4314e09eeaee
Alezh <47791363+Alezh89@users.noreply.github.com>
2019-06-10 17:11:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App.js';
 
+require('dotenv').config();
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
